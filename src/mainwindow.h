f4868bf545255acbc486cd970754be2c3967ac9a
LailaPOLY <113088501+LailaPOLY@users.noreply.github.com>
2023-01-18 17:32:48 +0100

    Update mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -7,6 +7,16 @@
 #include <time.h>
 using namespace std;
 #include <string>
+#include <vector>
+#include <QChartView>
+#include <QChart>
+#include <QLineSeries>
+#include <QValueAxis>
+#include <QFileDialog>
+#include <QDebug>
+#include <QPushButton>
+#include <QVBoxLayout>
+
 
 
 #include <QMainWindow>
@@ -24,9 +34,25 @@ public:
      ~MainWindow();
     int lecture();
     void ShowChart();
+    void vecteur();
+    QChartView *fenetre1; // un widget pour afficher un graphe
+    QChart *graphe1; // la reprÃ©sentation d'un graphe
+    QChart *graphe2;// la reprÃ©sentation d'un graphe
+    QChart *graphe3;
+    QLineSeries *donnees;// les donnÃ©es
+    QLineSeries *donnees2;
+    QLineSeries *donnees3;
+    QValueAxis *axisX;
+    QValueAxis *axisY;
+    
 
 private slots:
     void on_pushButton_clicked();
+    void on_actionOuvrir_triggered();
+    void on_graphique_1_clicked();
+    void on_graphique_2_clicked();
+    void on_graphique_3_clicked();
+    
 
 private:
     Ui::MainWindow *ui;
