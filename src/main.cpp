70821baa4f003a94388a886ec75ce152df4c67aa
cdourey <113088840+cdourey@users.noreply.github.com>
2023-02-20 20:00:05 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w; 
-    //vector<vector<string>> segments = w.splitString("C:/Users/Laila/OneDrive/Documents/Data/22-03-14 - Fluo CERMEP/Acq01.txt",'\t');
     w.show();
     return a.exec();
 }
